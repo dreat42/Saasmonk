224f50ad4accce7fff41b928719dcc5498cf11e3
Shagun Sethi <72696242+dreat42@users.noreply.github.com>
2024-10-07 16:31:11 +0530

    Update route.ts

--- src/app/api/getMoviesWithReviews/route.ts
+++ src/app/api/getMoviesWithReviews/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function POST() {
   try {
     const movies = await prisma.movie.findMany({
       include: {
